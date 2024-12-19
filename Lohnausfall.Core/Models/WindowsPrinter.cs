56e28f0799fd4de88e197c5c2734913f11228493
netFreezy <puettmann.f@gmail.com>
2024-12-19 12:20:39 +0100

    Drucker Seite angepasst mit Drucker Status

--- Lohnausfall.Core/Models/WindowsPrinter.cs
+++ Lohnausfall.Core/Models/WindowsPrinter.cs
@@ -1,9 +1,13 @@
-﻿namespace Lohnausfall.Core.Models
+﻿using System.Drawing;
+
+namespace Lohnausfall.Core.Models
 {
     public class WindowsPrinter (int id, string name, bool isDefaultPrinter = false)
     {
         public int Id { get; set; } = id;
         public string Name { get; set; } = name;
         public bool IsDefaultPrinter { get; set; } = isDefaultPrinter;
+        public bool IsOnline { get; set; } = false;
+        public string Status { get; set; } = "";
     }
 }
